8f5fe27c4318009f31d35d819d8f068bfa375228
Tarique-web <52395663+Tarique-web@users.noreply.github.com>
2020-11-18 15:02:06 +0530

    Update Insertion_sort.py

--- Insorting Sort/Insertion_sort.py
+++ Insorting Sort/Insertion_sort.py
@@ -1,11 +1,13 @@
-list1=[5,4,10,1,6,2]
-for i in range(1,len(list1)):
-	temp = list1[i]
-	j=i-1
-	while j>=0 and list1[j]>temp:
-		list1[j+1] = list1[j]
-		j-=1
-	list1[j+1]=temp
-	print(list1)
+def insertion(list1):
+	for i in range(1,len(list1)):
+		temp = list1[i]
+		j=i-1
+		while j>=0 and list1[j]>temp:
+			list1[j+1] = list1[j]
+			j-=1
+		list1[j+1]=temp
+	return list1
 
+list1 = list(map(int,input().split()))
 
+print(insertion(list1))
